54b438251f61fd85e0b3f667ea143114d2f541f6
chenyongda <605529354@qq.com>
2021-12-01 02:25:13 +0800

    修改端口

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,14 @@
 const path = require('path')
 module.exports = {
-    // 全局less变量
+  // 全局less变量
   pluginOptions: {
-  "style-resources-loader": {
-    preProcessor: "less",
+    "style-resources-loader": {
+      preProcessor: "less",
       // patterns: [path.resolve(__dirname, './src/assets/less/1.less')]
       patterns: []
+    },
+  },
+  devServer: {
+    port: 8123,
   }
-}
 };
